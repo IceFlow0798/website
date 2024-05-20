76325b4dadc268e560dd786f769bd6b31643fbe9
IceFlow <iceflow0798@outlook.com>
2024-05-20 14:39:50 +0800

    2024.5.20 modified

--- .eleventy.js
+++ .eleventy.js
@@ -2,45 +2,70 @@ const { DateTime } = require('luxon')
 const navigationPlugin = require('@11ty/eleventy-navigation')
 const rssPlugin = require('@11ty/eleventy-plugin-rss')
 
-
 module.exports = (config) => {
-  config.addPlugin(navigationPlugin);
-  config.addPlugin(rssPlugin);
+    config.addPlugin(navigationPlugin);
+    config.addPlugin(rssPlugin);
 
-  config.addPassthroughCopy('css');
-  config.addPassthroughCopy('static');
+    config.addPassthroughCopy('css');
+    config.addPassthroughCopy('static');
+    config.addPassthroughCopy("posts/pictures");
+    config.addPassthroughCopy("extensions/pictures");
   
-  config.setDataDeepMerge(true);
+    config.setDataDeepMerge(true);
 
-  config.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
-
-  config.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL, yyyy");
-  });
-
-  config.addCollection("tagList", collection => {
-    const tagsObject = {}
-    collection.getAll().forEach(item => {
-      if (!item.data.tags) return;
-      item.data.tags
-        .filter(tag => !['post', 'all'].includes(tag))
-        .forEach(tag => {
-          if(typeof tagsObject[tag] === 'undefined') {
-            tagsObject[tag] = 1
-          } else {
-            tagsObject[tag] += 1
-          }
-        });
+    config.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
     });
 
-    const tagList = []
-    Object.keys(tagsObject).forEach(tag => {
-      tagList.push({ tagName: tag, tagCount: tagsObject[tag] })
-    })
-    return tagList.sort((a, b) => b.tagCount - a.tagCount)
+    config.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL, yyyy");
+    });
 
-  });
+    config.addCollection("tagList", collection => {
+        const tagsObject = {}
+        collection.getAll().forEach(item => {
+            if (!item.data.tags) return;
+            item.data.tags
+            .filter(tag => !['post', 'all'].includes(tag))
+            .forEach(tag => {
+                if(typeof tagsObject[tag] === 'undefined') {
+                    tagsObject[tag] = 1
+                } else {
+                    tagsObject[tag] += 1
+                }
+            });
+        });
+        const tagList = []
+        Object.keys(tagsObject).forEach(tag => {
+            tagList.push({ tagName: tag, tagCount: tagsObject[tag] })
+        })
+        return tagList.sort((a, b) => b.tagCount - a.tagCount)
+    });
+
+    config.addCollection("categoryList", collection => {
+        const categoriesObject = {};
+        collection.getAll().forEach(item => {
+            if (!item.data.categories) return;
+            item.data.categories
+            .filter(categories => !['extensions', 'all'].includes(categories))
+            .forEach(category => {
+                if(typeof categoriesObject[category] === 'undefined') {
+                    categoriesObject[category] = { categoryName: category, categoryCount: 1, posts: [item] };
+                } else {
+                    categoriesObject[category].categoryCount++;
+                    categoriesObject[category].posts.push(item);
+                }
+            });
+        });
+        const categoryList = [];
+        Object.keys(categoriesObject).forEach(category => {
+            categoryList.push({
+                categoryName: category,
+                categoryCount: categoriesObject[category].categoryCount,
+                posts: categoriesObject[category].posts
+            });
+        });
+        return categoryList.sort((a, b) => b.categoryCount - a.categoryCount);
+    });
 
 }
---
+++ extensions/AutoRenameTag.md
@@ -0,0 +1,10 @@
+---
+    title: Auto Rename Tag
+    categories: ["HTML","标签","自动修改"]
+    date: 2024-03-20
+    icon: "/pictures/icons/AutoRenameTag.png"
+---
+
+该扩展修改HTML标签时，自动修改匹配的标签。
+
+![修改](../pictures/operations/autoRenameTag_change.gif)
